bdd6ede8b24efe285433bd333dd3ac86f8f6e4b0
Daniel Kronovet <kronovet@gmail.com>
2025-11-14 21:55:12 -0800

    fix: rename AdventureApp to App

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 
 import { StarknetProvider } from '@/components/StarknetProvider'
 
-import AdventureApp from '@/AdventureApp'
+import App from '@/App'
+// import MapTest from '@/pages/MapTest'
 import '@/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StarknetProvider>
-      <AdventureApp />
+      <App />
     </StarknetProvider>
   </StrictMode>,
 )
